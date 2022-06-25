d665a3cb7a9fca31e66485d3a8545d65e2ebe6c8
rishabhAjay <rishabhajay24@gmail.com>
2022-06-25 18:40:29 +0530

    added chat app socket server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const PORT = process.env.PORT || 5000
 
 const io = require("socket.io")(PORT, {
   cors: {
-    origin: "https://chat-app24.netlify.app",
+    origin: "client_URL",
   },
 });
 let users = [];
